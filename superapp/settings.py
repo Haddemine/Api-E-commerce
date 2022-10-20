17bc8ff975d94d1fa9dcceea2dee1929faf55c49
Haddemine <Haddemin1234@gmail.com>
2022-10-20 10:47:20 +0000

    k

--- superapp/settings.py
+++ superapp/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "django.contrib.sites",
     "whitenoise.runserver_nostatic",
-    "corsheaders"
+    "corsheaders",
     
     
     'ecommerce',
